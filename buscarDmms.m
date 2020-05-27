67e09c07bbb4bd4348fefbd1976d61be2cbe2aed
Pau <paujordanoliveras@gmail.com>
2020-05-27 11:01:42 +0200

    Implementat containers, afegit mainv2

---
+++ buscarDmms.m
@@ -0,0 +1,8 @@
+function dmms_sel = buscarDmms(dmms, clau)
+dmms_sel = []
+for dmm = dmms
+    if isKey(dmm.M, clau)
+        dmms_sel = [dmms_sel, dmm];
+    end
+end
+
---
+++ mesuresDict.m
@@ -0,0 +1,13 @@
+function [M] = mesuresDict(dmms)
+M = containers.Map('KeyType','char','ValueType','any');
+for dmm = dmms
+    for mag = dmm.magnitud
+        mag_key = char(mag.nom)
+        if isKey(M, mag_key)
+            M(mag_key) = [M(mag_key), mag];
+        else
+            M(mag_key) = mag;
+        end
+    end
+end
+
