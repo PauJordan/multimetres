2963b057d9d7e6076891b33c0af8dc4cb3755f7b
Pau <paujordanoliveras@gmail.com>
2020-05-27 12:10:22 +0200

    Selecció per mesura global feta

--- buscarDmms.m
+++ buscarDmms.m
@@ -1,5 +1,5 @@
 function dmms_sel = buscarDmms(dmms, clau)
-dmms_sel = []
+dmms_sel = [];
 for dmm = dmms
     if isKey(dmm.M, clau)
         dmms_sel = [dmms_sel, dmm];
--- mesuresDict.m
+++ mesuresDict.m
@@ -2,7 +2,7 @@ function [M] = mesuresDict(dmms)
 M = containers.Map('KeyType','char','ValueType','any');
 for dmm = dmms
     for mag = dmm.magnitud
-        mag_key = char(mag.nom)
+        mag_key = char(mag.nom);
         if isKey(M, mag_key)
             M(mag_key) = [M(mag_key), mag];
         else
